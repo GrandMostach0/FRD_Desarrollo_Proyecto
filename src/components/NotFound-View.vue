4cbff47c998632a55cfab751a0c1994d120dc4fd
vbchan <vbchan@frd.org.mx>
2025-11-02 21:01:30 -0600

    vista opcional de error de navegacion agregado como extra

---
+++ src/components/NotFound-View.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen text-center cursor-default">
+    <h1 class="text-5xl font-bold mb-6">404 - Ruta no encontrada 😿</h1>
+    <router-link 
+      to="/" 
+      class="text-3xl text-blue-600"
+    >
+      Volver al inicio
+    </router-link>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import OperadorView from '../components/Operador-Ventana.vue';
 import AdiosTap from "../components/Administrador-Ventana.vue";
 import Formulario from "../components/Formulario.vue";
 import InputView from "../components/Input-view.vue";
+import NotFoundView from "../components/NotFound-View.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
             path: '/adios',
             name: 'Adios',
             component: AdiosTap
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'NotFound',
+            component: NotFoundView
         }
     ]
 })
